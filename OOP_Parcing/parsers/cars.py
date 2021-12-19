bb0021eb538cc2f16f722bcadc13e177e20ce19e
Anton Jegorov <94009242+ignore87@users.noreply.github.com>
2021-12-19 20:17:24 +0200

    Create cars.py

---
+++ OOP_Parcing/parsers/cars.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from modules.MainParser import MainParser
+
+class Cars(MainParser):
+    def __init__(self, url):
+        self.url = url
+        self.items_list = []
+    
+    def parse(self):
+        response = requests.get(self.url)
+        html = bs(response.content, 'html.parser')
+        for el in html.select('.nbl-slimPosterBlock__title'):
+            self.items_list.append(el.text)
+        return self.items_list
